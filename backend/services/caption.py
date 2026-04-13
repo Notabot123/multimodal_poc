9a7ab87087568c5b6ccb4f24c9ad49ecacb8b9d6
Notabot123 <37666014+Notabot123@users.noreply.github.com>
2026-04-13 11:23:05 +0100

    fallback openai for transcript and captions for now

--- backend/services/caption.py
+++ backend/services/caption.py
@@ -5,7 +5,8 @@ MODE = os.getenv("EMBEDDING_MODE", "mock")
 if MODE == "openai":
     from services.providers.openai_provider import describe_image
 elif MODE == "gemini":
-    # try transcribe feature with gemini
-    from services.providers.gemini_provider import describe_image
+    # try transcribe feature with gemini. fallback on openAI for now as flash in high demand
+    #from services.providers.gemini_provider import describe_image
+    from services.providers.openai_provider import describe_image
 else:
     from services.providers.mock_provider import describe_image
--- backend/services/speech.py
+++ backend/services/speech.py
@@ -5,7 +5,8 @@ MODE = os.getenv("EMBEDDING_MODE", "mock")
 if MODE == "openai":
     from services.providers.openai_provider import transcribe_audio
 elif MODE == "gemini":
-    # tryno transcribe feature with gemini
-    from services.providers.gemini_provider import transcribe_audio
+    # fallback openAI for now as flash in high demand
+    #from services.providers.gemini_provider import transcribe_audio
+    from services.providers.openai_provider import transcribe_audio
 else:
     from services.providers.mock_provider import transcribe_audio
